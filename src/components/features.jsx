906f3eef2c657e417bdf5cdd23a9588df7ff9835
lizjia <79896415+lizjia@users.noreply.github.com>
2021-10-17 21:39:07 -0400

    Add files via upload

---
+++ src/components/features.jsx
@@ -0,0 +1,15 @@
+export const Features = (props) => {
+  return (
+    <div id='features' className='text-center'>
+      <div className='container'>
+        <div className='col-md-10 col-md-offset-1 section-title'>
+          <h2>Features</h2>
+        </div>
+        <div className='row'>
+        <iframe src="https://calendar.google.com/calendar/embed?src=sshb5he2u8ak5t72cjdu7dgefc%40group.calendar.google.com&ctz=America%2FNew_York"></iframe>
+
+        </div>
+      </div>
+    </div>
+  )
+}
