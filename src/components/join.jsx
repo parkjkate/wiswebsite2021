98d313a0cd70c41a56ad8ded49d6b2b9e4b0a0eb
Kate Park <jpark743@uwo.ca>
2021-10-27 13:36:41 -0400

    initial

--- src/components/join.jsx
+++ src/components/join.jsx
@@ -10,7 +10,8 @@ export const Join = (props) => {
         </p>
         <div className='row'>
           <>
-            <Button variant="primary" size="lg" href = 'https://docs.google.com/forms/d/1ghO2tVJLUGkmqyJnHG9dHQAdMSg6LdmlHSmpyYeSvrM/viewform?edit_requested=true&fbclid=IwAR3H7Ucc2sMvQusR0Ln45Bx8oKUF9u1p2dVSPVtitjouGodRCWyiAm8Z1EE'>
+
+            <Button className='btn btn-custom btn-lg' href = 'https://docs.google.com/forms/d/1ghO2tVJLUGkmqyJnHG9dHQAdMSg6LdmlHSmpyYeSvrM/viewform?edit_requested=true&fbclid=IwAR3H7Ucc2sMvQusR0Ln45Bx8oKUF9u1p2dVSPVtitjouGodRCWyiAm8Z1EE'>
               Join our Team.
             </Button>{''}
           </>
